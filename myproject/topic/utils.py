6561003a056732567a4dfd47929630676ea4518f
zabni1 <nikitasolodovnik763@gmail.com>
2026-04-29 19:29:21 +0300

    Massage: New commit

--- myproject/topic/utils.py
+++ myproject/topic/utils.py
@@ -6,7 +6,7 @@ from .models import Comment, Likes, Reply
 
 class DataMixin:
     def get_mixin_context(self, context, email, **kwargs):
-        context['likes_com'] = Likes.objects.filter(email=email).values_list('email', flat=True)
+        context['likes_com'] = Likes.objects.filter(email=email).values_list('comment_id', flat=True)
         context['comment_user'] = Comment.objects.filter(email=email).values_list('pk', flat=True)
         context.update(kwargs)
         return context
@@ -15,7 +15,7 @@ class DataMixin:
         topic_id = pk
         comment = Comment.objects.filter(topic_id=pk).annotate(all_likes=Count('likes'),
                                                                 all_replies=Count('replies', distinct=True)).order_by('created_at')
-        likes_com = Likes.objects.filter(email=email).values_list('email', flat=True)
+        likes_com = Likes.objects.filter(email=email).values_list('comment_id', flat=True)
         comment_user = Comment.objects.filter(email=email).values_list('pk', flat=True)
         context = {
             'topic_id': topic_id,
@@ -27,7 +27,7 @@ class DataMixin:
 
     def get_context_replies(self, pk, email):
         replies = Reply.objects.filter(comment_id=pk).annotate(all_likes=Count('likes')).select_related('comment')
-        likes_rep = Likes.objects.filter(email=email).values_list('email', flat=True)
+        likes_rep = Likes.objects.filter(email=email).values_list('reply_id', flat=True)
         reply_user = Reply.objects.filter(email=email).values_list('pk', flat=True)
         context = {
             'pk': pk,
